8a4c79ad552c85280fd646fa9fe12fb30c6deaf4
Taki <irivaldogomes34@gmail.com>
2026-03-04 17:55:55 -0300

    Adiciona persistência de dados com JSON

--- main.py
+++ main.py
@@ -1,36 +1,69 @@
-tarefas = []
-proximo_id = 1
+import json
 
-print("=== TASK MANAGER ===")
-print("1 - Adicionar tarefa")
-print("2 - Listar tarefas")
-print("3 - Sair")
+def salvar_tarefas(tarefas):
+    with open("tarefas.json", "w", encoding="utf-8") as arquivo:
+        json.dump(tarefas, arquivo, ensure_ascii=False, indent=4)
 
-opcao = input("Escolha uma opção: ")
-
-if opcao == "1":
-    descricao = input("Digite a descrição da tarefa: ")
+def carregar_tarefas():
+    try:
+        with open("tarefas.json", "r", encoding="utf-8") as arquivo:
+            return json.load(arquivo)
+    except FileNotFoundError:
+        return []
     
+def gerar_proximo_id(tarefas):
+    if not tarefas:
+        return 1
+    return max(tarefa["id"] for tarefa in tarefas) + 1
+
+def adicionar_tarefa(tarefas):
+    descricao = input("Digite a descrição da tarefa: ")
+
     nova_tarefa = {
-        "id": proximo_id,
+        "id": gerar_proximo_id(tarefas),
         "descricao": descricao,
         "concluida": False
     }
 
     tarefas.append(nova_tarefa)
-    proximo_id += 1
+    salvar_tarefas(tarefas)
 
+    print("Tarefa adicionada com sucesso.\n")
 
-    print("Tarefa adicionada com sucesso.")
-
-elif opcao == "2":
-    print("Lista de tarefas:")
-    for tarefa in tarefas:
-        status = "✔" if tarefa["Concluida"] else "✘"
-        print(f'{tarefa["id"]} - {tarefa["descricao"]} [{status}]')
-
-elif opcao == "3":
-    print("Saindo...")
+def listar_tarefas(tarefas):
+    if not tarefas:
+        print("Nenhuma tarefa adicionada.\n")
+        return 
     
-else:
-    print("Opção inválida.")
+    print("\nLista de tarefas: ")
+    for tarefa in tarefas:
+        status = "✔" if tarefa["concluida"] else "✘"
+        print(f'{tarefa["id"]} - {tarefa["descricao"]} [{status}]')
+    print()
+
+def main():
+    tarefas = carregar_tarefas()
+
+    while True:
+        print("=== TASK MANAGER ===")
+        print("1 - Adicionar tarefa")
+        print("2 - Listar tarefas")
+        print("3 - Sair")
+
+        opcao = input("Escolha uma opção: ")
+
+        if opcao == "1":
+            adicionar_tarefa(tarefas)
+
+        elif opcao == "2":
+            listar_tarefas(tarefas)
+
+        elif opcao == "3":
+            print("Saindo...")
+            break
+
+        else:
+            print("Opção inválida.\n")
+
+if __name__=="__main__":
+    main()
